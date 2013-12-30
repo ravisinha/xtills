14d9027409ada45431ac5c9a5fe0b0c76073f307
Ravi Sinha <ravi.rakesh@gmail.com>
2013-12-30 21:00:56 +0000

    Customise Bootstrap vars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Xtills::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" #creates a variable called about_path
-  
+  get "team" => "pages#team" #creates team_path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
