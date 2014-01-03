6dc38891e65cfc7371cc4c43061af71e384cd2ba
Ravi Sinha <ravi.rakesh@gmail.com>
2014-01-03 22:03:24 +0000

    Add Amazon S3 for paperclip

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap-sass', '~> 3.0.3.0'
 #gem 'bootstrap-sass', github: 'thomas-macdonald/bootstrap-sass', branch: '3'
 gem 'devise', '~>3.2.2'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk', '~>1.20.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,16 @@ Xtills::Application.configure do
   # Required for Heroku
   # set this to actual host name
   config.action_mailer.default_url_options = { :host => 'xtills.com' }
+
+  # config/environments/production.rb
+  
+  #This sets paperclip images on Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
